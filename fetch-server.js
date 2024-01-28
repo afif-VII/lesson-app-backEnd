c0a15c10ac3fcf60cbf8e36621b1f679bea04943
Afif <81876281+afif-VII@users.noreply.github.com>
2024-01-28 20:03:30 +0000

    Add fs

--- fetch-server.js
+++ fetch-server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
+const fs = require('fs');
+
 
 let app = express();
 app.set('json spaces', 3);
